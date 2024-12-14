09c61c80b641ec08bfc42661a6215f84261a30c4
kumar <krksingh.99@gmail.com>
2024-12-14 23:58:05 +0530

    timeout and interval added

--- node-essential/server.js
+++ node-essential/server.js
@@ -3,12 +3,35 @@ const serverRunner  = require('./serverRunner');
 
 const CmdCammend= process.argv;
 const builderCmd = CmdCammend[2].includes('serverBuild')? CmdCammend[2].split('='):undefined;
-console.log(builderCmd)
 
-if(builderCmd[0] ==='--serverBuild' && builderCmd[1] ==='true'){
-    serverBuilder();
-}else{
-    serverRunner();
+const waitTime = 3000;
+process.stdout.write('Wailt for System to Run');
+
+const timeFinished =  () => {
+    process.stdout.write('\nRuning Your App')
+    clearInterval(intervalSub)
+    app()
+}
+
+setTimeout(timeFinished, waitTime);
+const waitInterval =500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    let percentage = Math.floor((currentTime/waitTime)*100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`system restart ${percentage}%`)
+
+}
+let intervalSub =setInterval(incTime,waitInterval)
+const app = () => {
+    if(builderCmd[0] ==='--serverBuild' && builderCmd[1] ==='true'){
+        serverBuilder();
+    }else{
+        serverRunner();
+    }
 }
 
 
